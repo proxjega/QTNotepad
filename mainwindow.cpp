d820073cba8df5fd348b04c0724ed4aa8d73ed3f
Nikita Jegorov <proxjega@gmail.com>
2025-06-11 02:30:28 +0300

    edit sector

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,8 @@
 #include <QtPrintSupport/QPrinter>
 #include <QtPrintSupport/QPrintDialog>
 #include <QPageSetupDialog>
+#include <QInputDialog>
+#include <QDesktopServices>
 //#include <string>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -38,10 +40,11 @@ void MainWindow::on_actionSave_triggered() {
         out << this->ui->textEdit->toPlainText();
         file.close();
         QMessageBox::information(this, tr("File Saved"), tr("File saved successfully!"));
+        this->setWindowTitle(getTitle(_filename));
+        _saved = true;
     }
     //set title
-    this->setWindowTitle(getTitle(_filename));
-    _saved = true;
+
 }
 
 void MainWindow:: on_actionSave_as_triggered() {
@@ -130,6 +133,107 @@ void MainWindow::on_textEdit_textChanged() {
 void MainWindow:: on_actionWord_Wrap_triggered(bool checked){
     if(checked == true) this->ui->textEdit->setLineWrapMode(QTextEdit::WidgetWidth);
     else this->ui->textEdit->setLineWrapMode(QTextEdit::NoWrap);
+}
+//edit
+void MainWindow::on_actionChoose_everything_triggered() {
+    this->ui->textEdit->selectAll();
+}
+
+void MainWindow::on_actionCut_triggered() {
+    this->ui->textEdit->cut();
+}
+
+void MainWindow::on_actionCopy_triggered() {
+    this->ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered() {
+    this->ui->textEdit->paste();
+}
+
+void MainWindow::on_actionUndo_triggered() {
+    this->ui->textEdit->undo();
+}
+
+void MainWindow::on_actionDelete_triggered() {
+    this->ui->textEdit->textCursor().removeSelectedText();
+}
+void MainWindow::on_actionFind_triggered() {
+    QString searchText = QInputDialog::getText(this, tr("Find"), tr("Enter text to find:"));
+    if (!searchText.isEmpty()) {
+        QTextCursor cursor = this->ui->textEdit->textCursor();
+        cursor = this->ui->textEdit->document()->find(searchText, cursor);
+        if (!cursor.isNull()) {
+            this->ui->textEdit->setTextCursor(cursor);
+        } else {
+            QMessageBox::information(this, tr("Not Found"), tr("Text not found."));
+        }
+    }
+}
+
+void MainWindow::on_actionFind_next_triggered() {
+    QString searchText = QInputDialog::getText(this, tr("Find Next"), tr("Enter text to find:"));
+    if (!searchText.isEmpty()) {
+        QTextCursor cursor = this->ui->textEdit->textCursor();
+        cursor = this->ui->textEdit->document()->find(searchText, cursor);
+        if (!cursor.isNull()) {
+            this->ui->textEdit->setTextCursor(cursor);
+        } else {
+            QMessageBox::information(this, tr("Not Found"), tr("Text not found."));
+        }
+    }
+}
+
+void MainWindow::on_actionFind_previous_triggered() {
+    QString searchText = QInputDialog::getText(this, tr("Find Previous"), tr("Enter text to find:"));
+    if (!searchText.isEmpty()) {
+        QTextCursor cursor = this->ui->textEdit->textCursor();
+        cursor = this->ui->textEdit->document()->find(searchText, cursor, QTextDocument::FindBackward);
+        if (!cursor.isNull()) {
+            this->ui->textEdit->setTextCursor(cursor);
+        } else {
+            QMessageBox::information(this, tr("Not Found"), tr("Text not found."));
+        }
+    }
+}
+
+void MainWindow::on_actionDate_and_time_triggered() {
+    QString dateTime = QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss");
+    this->ui->textEdit->insertPlainText(dateTime);
+}
+
+void MainWindow::on_actionGo_to_triggered() {
+    bool ok;
+    int lineNumber = QInputDialog::getInt(this, tr("Go to Line"), tr("Line number:"), 1, 1, this->ui->textEdit->document()->lineCount(), 1, &ok);
+    if (ok) {
+        QTextCursor cursor = this->ui->textEdit->textCursor();
+        cursor.movePosition(QTextCursor::Start);
+        cursor.movePosition(QTextCursor::Down, QTextCursor::MoveAnchor, lineNumber - 1);
+        this->ui->textEdit->setTextCursor(cursor);
+    }
+}
+
+void MainWindow::on_actionReplace_triggered() {
+    QString findText = QInputDialog::getText(this, tr("Find"), tr("Enter text to find:"));
+    if (findText.isEmpty()) return;
+
+    QString replaceText = QInputDialog::getText(this, tr("Replace"), tr("Enter text to replace with:"));
+    if (replaceText.isEmpty()) return;
+
+    QTextDocument *document = this->ui->textEdit->document();
+    QTextCursor cursor = document->find(findText);
+    while (!cursor.isNull()) {
+        cursor.insertText(replaceText);
+        cursor = document->find(findText, cursor);
+    }
+}
+
+void MainWindow::on_actionSearch_with_Bing_triggered() {
+    QString searchText = this->ui->textEdit->textCursor().selectedText();
+    if (!searchText.isEmpty()) {
+        QString url = QString("https://www.bing.com/search?q=%1").arg(QUrl::toPercentEncoding(searchText));
+        QDesktopServices::openUrl(QUrl(url));
+    }
 }
 
 void MainWindow::closeEvent(QCloseEvent *event) {
--- mainwindow.h
+++ mainwindow.h
@@ -29,6 +29,21 @@ private slots:
     void on_actionFont_triggered();
     void on_actionPrint_triggered();
     void on_textEdit_textChanged();
+    //edit
+    void on_actionChoose_everything_triggered();
+    void on_actionCut_triggered();
+    void on_actionCopy_triggered();
+    void on_actionPaste_triggered();
+    void on_actionUndo_triggered();
+    void on_actionDate_and_time_triggered();
+    void on_actionDelete_triggered();
+    void on_actionFind_triggered();
+    void on_actionFind_next_triggered();
+    void on_actionFind_previous_triggered();
+    void on_actionGo_to_triggered();
+    void on_actionReplace_triggered();
+    void on_actionSearch_with_Bing_triggered();
+    //view
     void on_actionWord_Wrap_triggered(bool checked);
     void closeEvent(QCloseEvent *event) override;
 
